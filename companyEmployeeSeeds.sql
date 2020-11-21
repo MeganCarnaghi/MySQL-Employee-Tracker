8ad7dd01f3bcfb913b0525a7de8014865cd1c3a1
Megan Carnaghi <carnagh5@gmail.com>
2020-11-21 14:00:18 -0500

    Updates to companyEmployeeSeeds.sql file

--- companyEmployeeSeeds.sql
+++ companyEmployeeSeeds.sql
@@ -33,4 +33,16 @@ PRIMARY KEY (id),
 FOREIGN KEY (department_id)
 );
 
+-- Insert data into the employee table
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ("", "", "");
+
+-- Insert data into the department table
+INSERT INTO department (name)
+VALUES ("", "", "");
+
+-- Insert data into the role table
+INSERT INTO role (title, salary, department_id)
+VALUES ("", "", "");
+
 
