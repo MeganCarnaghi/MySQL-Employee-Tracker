6534ddb59a3b6a3bbe2f42baa06513efd8e2bb75
Megan Carnaghi <carnagh5@gmail.com>
2020-11-22 19:20:38 -0500

    Updates to schema and seed files

--- employeeTrackerSchema.sql
+++ employeeTrackerSchema.sql
@@ -12,7 +12,8 @@ last_name VARCHAR(30),
 role_id INTEGER(10),
 manager_id INTEGER(10),
 PRIMARY KEY (id),
-FOREIGN KEY (role_id) REFERENCES role(id)
+FOREIGN KEY (role_id) REFERENCES role(id),
+FOREIGN KEY (manager_id) REFERENCES employee (id)
 );
 
 
--- employeeTrackerSeeds.sql
+++ employeeTrackerSeeds.sql
@@ -2,7 +2,31 @@ USE employee_trackerDB;
 
 -- Insert data into the employee table
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUES ("", "", "");
+VALUES ("Karen", "Short", "1", "9");
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ("Lisa", "Matthews", "2", "1");
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ("Jim", "Harding", "3", "9");
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ("Edward", "Smith", "4", "3");
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ("Tina", "Barclay", "5", "null");
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ("Alex", "Nash", "6", "5");
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ("Wendy", "Curtis", "7", "3");
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ("Natalie", "Sutka", "8", "7");
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ("Megan", "Carnaghi", "9", "null");
 
 -- Insert data into the department table
 INSERT INTO department (name)
@@ -18,35 +42,32 @@ INSERT INTO department (name)
 VALUES ("Customer Service");
 
 -- Insert data into the role table
-INSERT INTO role (title, salary, department_id)
-VALUES ("manager", 100000, 1);
 
--- Insert data into the role table
 INSERT INTO role (title, salary, department_id)
-VALUES ("social media specialist", 60000, 1);
+VALUES ("Director of Marketing", 100000, 1);
 
--- Insert data into the role table
 INSERT INTO role (title, salary, department_id)
-VALUES ("manager", 100000, 2);
+VALUES ("Social Media Specialist", 60000, 1);
 
--- Insert data into the role table
 INSERT INTO role (title, salary, department_id)
-VALUES ("account executive", 80000, 2);
+VALUES ("Director of Sales", 100000, 2);
 
--- Insert data into the role table
 INSERT INTO role (title, salary, department_id)
-VALUES ("manager", 100000, 3);
+VALUES ("Account Executive", 80000, 2);
 
--- Insert data into the role table
 INSERT INTO role (title, salary, department_id)
-VALUES ("full stack developer", "90000", 3);
+VALUES ("Egineering Manager", 100000, 3);
 
--- Insert data into the role table
 INSERT INTO role (title, salary, department_id)
-VALUES ("manager", "100000", 4);
+VALUES ("Full Stack Developer", "90000", 3);
 
--- Insert data into the role table
 INSERT INTO role (title, salary, department_id)
-VALUES ("customer service specialist", "50000", 4);
+VALUES ("Customer Service Manager", "100000", 4);
+
+INSERT INTO role (title, salary, department_id)
+VALUES ("Customer Service Specialist", "50000", 4);
+
+INSERT INTO role (title, salary, department_id)
+VALUES ("CEO", "500000", "null");
 
 
