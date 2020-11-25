a3b2b385fcafd7e26ae9b05abd6e247c5499f096
Megan Carnaghi <carnagh5@gmail.com>
2020-11-24 20:10:27 -0500

    updates to server.js file

--- server.js
+++ server.js
@@ -18,8 +18,7 @@ connection.connect(function(err) {
 });
 
 function promptUser(){
-    inquirer.prompt([
-      {
+    inquirer.prompt({
         type: "list",
         name: "action",
         message: "Do would you like to do?",
@@ -30,15 +29,65 @@ function promptUser(){
           "Add An Employee",
           "Remove An Employee",
           "Update An Employee's Role",
-          "Update An Employee's Manager"
+          "Update An Employee's Manager",
+          "Quit"
         ]
+    })
+    .then(function(answer) {
+      switch(answer.action) {
+
+        case "View All Employees":
+        viewAllEmployees();
+        break;
+
+        case "View All Employees By Department":
+        viewEmpByDept();
+        break;
+
+        case "View All Employees By Manager":
+        viewEmpByMgr();
+        break;
+
+        case "Add An Employee":
+        addEmployee();
+        break;
+
+        case "Remove An Employee":
+        removeEmployee();
+        break;
+
+        case "Update An Employee's Role":
+        updateRole();
+        break;
+
+        case "Update An Employee's Manager":
+        updateManager();
+        break;
+
+        case "Exit":
+        connection.end();
+        break;
       }
-    ])
-    .then(function(data) {
-        if(data.action === "View All Employees") {
+    })
 
 
 // A function to display all of the employees
-function viewAllEmployees(){
+function viewAllEmployees()
 
-}
+// A function to display all employees by department
+function viewEmpByDept()
+
+// A function to display all employees by manager
+function viewEmpByMgr()
+
+// A function to add an employee
+function addEmployee()
+
+// A function to remove an employee
+function removeEmployee()
+
+// A function to update an employee's role
+function updateRole()
+
+// A function to update an employee's manager
+function updateManager()
