f0ae202c727d990d40609c6d333e52636d9e1ca2
Megan Carnaghi <carnagh5@gmail.com>
2020-11-27 16:09:49 -0500

    Update index.js

--- db/index.js
+++ db/index.js
@@ -5,21 +5,22 @@ class SQLqueries {
     constructor(connection) {
         this.connection = connection;
     }
-
+    
     selectEmployees() {
-        connection.query("SELECT * FROM employees", function(err, res) {
+        connection.query("SELECT employee.id AS ID, employee.first_name AS FIRST NAME, employee.last_name AS LAST NAME, role.title AS ROLE, department.name AS DEPARTMENT, role.salary AS SALARY, CONCAT(manager.first_name, ' ', manager.last_name) AS MANAGER FROM employee LEFT JOIN role on employee.role_id = role.id LEFT JOIN department on role.department_id = department.id LEFT JOIN employee manager on manager.id = employee.manager_id;"
+        , function(err, res) {
             if (err) throw err;
         });
     }
 
     selectDepartments() {
-        connection.query("SELECT * FROM departments", function(err, res) {
+        connection.query("SELECT id AS ID, name as DEPARTMENT FROM department", function(err, res) {
             if (err) throw err;
         });
     }
 
     selectRoles() {
-        connection.query("SELECT * FROM roles", function(err, res) {
+        connection.query("SELECT id AS ID, title as ROLE, salary as SALARY FROM role", function(err, res) {
             if (err) throw err;
         });
     }
